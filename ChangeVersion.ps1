5fb72b2df878c9b7a6b31e3acdd438915351ced0
Ladislav Prix <elix78963@gmail.com>
2020-04-03 14:45:51 +0200

    Removed useless script

--- ChangeVersion.ps1
+++
@@ -1,9 +0,0 @@
-param([string] $from, [string] $to)
-
-$configFiles = Get-ChildItem . *.csproj -rec
-foreach ($file in $configFiles)
-{
-    (Get-Content $file.PSPath -Encoding UTF8) |
-    Foreach-Object { $_ -replace "<Version>$from</Version>", "<Version>$to</Version>" } |
-    Set-Content $file.PSPath -Encoding UTF8
-}
