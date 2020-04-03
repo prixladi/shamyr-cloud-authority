9c9ae23e2d98782c7c3c4ceb5fcbc4141c44572a
Ladislav Prix <elix78963@gmail.com>
2020-04-03 14:44:02 +0200

    Added shamyr server swashbuckle

---
+++ src/Shamyr.Server.Swashbuckle/Filters/AuthorizedEndpointsOperationFilterBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace Shamyr.Server.Swashbuckle.Filters
+{
+  public abstract class AuthorizedEndpointsOperationFilterBase: AspNetCore.Swashbuckle.Filters.AuthorizedEndpointsOperationFilterBase
+  {
+    protected override IEnumerable<(int statusCode, string description)> GetStatuses()
+    {
+      yield return (StatusCodes.Status401Unauthorized, "User is not authorized");
+      yield return (CustomStatusCodes.Status430NotVerified, "Account email is not verified");
+      yield return (CustomStatusCodes.Status431UserDisabled, "User is disabled");
+    }
+  }
+}
